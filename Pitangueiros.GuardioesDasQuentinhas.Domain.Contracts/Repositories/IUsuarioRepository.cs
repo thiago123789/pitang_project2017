592c43978f8125647625e6605c58e2bac15f89d0
ydonasc <yurinascimentofarias@gmail.com>
2018-01-02 13:45:49 -0200

    Teste de alteração de usuário

--- Pitangueiros.GuardioesDasQuentinhas.Domain.Contracts/Repositories/IUsuarioRepository.cs
+++ Pitangueiros.GuardioesDasQuentinhas.Domain.Contracts/Repositories/IUsuarioRepository.cs
@@ -10,5 +10,6 @@ namespace Pitangueiros.GuardioesDasQuentinhas.Domain.Contracts.Repositories
     public interface IUsuarioRepository: IRepository<Usuario, long>
     {
         Usuario ObterPorLogin(string email);
+
     }
 }
