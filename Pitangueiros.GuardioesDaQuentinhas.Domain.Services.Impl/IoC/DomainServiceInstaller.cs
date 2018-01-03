00cb6867bb69de14d72680f3495c931a2e4ff7e6
ydonasc <yurinascimentofarias@gmail.com>
2018-01-03 10:22:07 -0200

    Registradas as interfaces de domain service no Castle

---
+++ Pitangueiros.GuardioesDaQuentinhas.Domain.Services.Impl/IoC/DomainServiceInstaller.cs
@@ -0,0 +1,27 @@
+﻿using Castle.MicroKernel.Registration;
+using Castle.MicroKernel.SubSystems.Configuration;
+using Castle.Windsor;
+using Pitangueiros.GuardioesDasQuentinhas.Domain.Contracts.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pitangueiros.GuardioesDasQuentinhas.Domain.Services.Impl.IoC
+{
+
+    public class DomainServiceInstaller : IWindsorInstaller
+    {
+        public void Install(IWindsorContainer container, IConfigurationStore store)
+        {
+            container.Register(
+                Component.For<IClienteService, ClienteService>(),
+                Component.For<ILojaService, LojaService>(),
+                Component.For<IPagamentoService, PagamentoService>(),
+                Component.For<IUsuarioService, UsuarioService>(),
+                Component.For<IVendedorService, VendedorService>()
+            );
+        }
+    }
+}
