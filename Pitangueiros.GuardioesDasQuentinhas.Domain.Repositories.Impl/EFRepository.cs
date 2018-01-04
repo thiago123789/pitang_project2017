dd88fb1c30cac4168cc0c9f05fce203139d5370b
Igor Formiga <igorformigaa@gmail.com>
2018-01-04 11:16:28 -0300

    Arrumando o arquivo do SQL, tentando mapear o banco e Criação da classe EFRepository

---
+++ Pitangueiros.GuardioesDasQuentinhas.Domain.Repositories.Impl/EFRepository.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Pitangueiros.GuardioesDasQuentinhas.Domain.Contracts.Repositories;
+using Pitangueiros.GuardioesDasQuentinhas.Domain.Entities;
+
+namespace Pitangueiros.GuardioesDasQuentinhas.Domain.Repositories.Impl
+{
+    //Lembrar perguntar BURGO SOBRE ISSO POR QUÊ TÁ DANDO PROBLEMA QUANDO USA A INTERFACE???
+    public class EFRepository<T,TId> : IRepository<T, TId> where T : EntidadeBase<TId>
+        where TId : IEquatable<TId>, IComparable
+    {
+        public void Delete(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T GetOne(TId entityId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<T> List()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Save(T entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Pitangueiros.GuardioesDasQuentinhas.Domain.Repositories.Impl/GuardioesDasQuentinhasDbContext.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using Pitangueiros.GuardioesDasQuentinhas.Domain.Entities;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Pitangueiros.GuardioesDasQuentinhas.Domain.Repositories.Impl
+{
+    public class GuardioesDasQuentinhasDbContext : DbContext{
+        public GuardioesDasQuentinhasDbContext() /*: base ("A gente n")*/{
+
+        }
+        public DbSet<Usuario> Usuarios { get; set; }
+
+        public DbSet<Cliente> Clientes { get; set; }
+
+        public DbSet<Vendedor> Vendedores { get; set; }
+
+        public DbSet<Loja> Lojas { get; set; }
+
+        public DbSet<Cartao> Cartoes { get; set; }
+
+        public DbSet<Porcao> Porcoes { get; set; }
+
+        public DbSet<Pagamento> Pagamentos { get; set; }
+
+        public DbSet<Comentario> Comentarios { get; set; }
+
+        public DbSet<Avaliacao> Avaliacoes { get; set; }
+
+        public DbSet<Pedido> Pedidos { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            //Mapeamento de Usuario
+            modelBuilder.Entity<Usuario>()
+                .HasKey(p => p.Id);
+            modelBuilder.Entity<Usuario>().Property(p => p.Id)
+                .HasDatabaseGeneratedOption(DatabaseGeneratedOption.Identity);
+            modelBuilder.Entity<Usuario>().Property(p => p.Nome)
+                .HasColumnName("Nome");
+            modelBuilder.Entity<Usuario>().Property(p => p.Email)
+                .HasColumnName("Email");
+            modelBuilder.Entity<Usuario>().Property(p => p.Senha)
+                .HasColumnName("Senhas");
+
+
+            base.OnModelCreating(modelBuilder);
+        }
+        //mapeamento
+    }
+}
