eecb8e44df6d2e2dc1b29da91ceff24856153703
alanyamos <amos@ecomp.poli.br>
2018-01-09 12:52:30 -0300

    EF (65%)

--- Pitangueiros.GuardioesDasQuentinhas.Domain.Repositories.Impl/Mapping/AvaliacaoMapping.cs
+++ Pitangueiros.GuardioesDasQuentinhas.Domain.Repositories.Impl/Mapping/AvaliacaoMapping.cs
@@ -5,10 +5,28 @@ using System.Text;
 using System.Threading.Tasks;
 using Pitangueiros.GuardioesDasQuentinhas.Domain.Entities;
 using System.Data.Entity.ModelConfiguration;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Pitangueiros.GuardioesDasQuentinhas.Domain.Repositories.Impl.Mapping
 {
     class AvaliacaoMapping : EntityTypeConfiguration<Avaliacao>
     {
+        public AvaliacaoMapping()
+        {
+            ToTable("Avaliacao");
+            this.HasKey(p => p.Id);
+            this.Property(p => p.Id)
+                .HasDatabaseGeneratedOption(DatabaseGeneratedOption.Identity);
+            this.Property(p => p.Nota)
+                .HasColumnName("Nota").IsRequired();
+            this.Property(p => p.DataAvaliacao)
+                .HasColumnName("DataAvaliacao").IsRequired();
+            this.Property(p => p.Comentario)
+                .HasColumnName("Comentario");
+            this.HasRequired<Cliente>(p => p.Cliente)
+                .WithMany();
+            this.HasRequired<Loja>(p => p.Loja)
+                .WithMany();
+        }
     }
 }
