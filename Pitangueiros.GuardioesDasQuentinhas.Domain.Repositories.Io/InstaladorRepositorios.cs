347785cb3a78326698c88b89698d0d1fafa17a56
thiago123789 <thiago123789@gmail.com>
2018-01-09 17:16:53 -0300

    Installer repositories IoC

---
+++ Pitangueiros.GuardioesDasQuentinhas.Domain.Repositories.Io/InstaladorRepositorios.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+using Pitangueiros.GuardioesDasQuentinhas.Cross.IoC.Contracts;
+using Pitangueiros.GuardioesDasQuentinhas.Domain.Repositories.Impl;
+
+namespace Pitangueiros.GuardioesDasQuentinhas.Domain.Repositories.IoC
+{
+    public class InstaladorRepositorios: IInicializadorIoC
+    {
+        public void Inicializar(IGerenciadorIoC gerenciadorIoC)
+        {
+            gerenciadorIoC.RegistrarAssemblyPorConvencao(Assembly.GetAssembly(typeof(UsuarioMemoryRepository)));
+        }
+    }
+}
