66eb20d389f6349b6ee4bbad3fc5cd8da56baf8f
Igor Formiga <igorformigaa@gmail.com>
2018-01-17 12:56:31 -0300

    Antes de hoje eu era ateu.

---
+++ Pitangueiros.GuardioesDasQuentinhas.App.Entities/PagamentoInputDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pitangueiros.GuardioesDasQuentinhas.App.Entities
+{
+    public class PagamentoInputDto
+    {
+    }
+}
