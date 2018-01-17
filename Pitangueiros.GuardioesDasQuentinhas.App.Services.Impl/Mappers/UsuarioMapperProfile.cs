3aa3bc15a3b4c377366cc8a43a9e051c3e10efc4
alanyamos <amos@ecomp.poli.br>
2018-01-17 08:54:03 -0300

    [TASK17] AutoMapper Usuarios

---
+++ Pitangueiros.GuardioesDasQuentinhas.App.Services.Impl/Mappers/UsuarioMapperProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using Pitangueiros.GuardioesDasQuentinhas.Cross.IoC.Contracts.Mapeador;
+using Pitangueiros.GuardioesDasQuentinhas.Domain.Entities;
+using Pitangueiros.GuardioesDasQuentinhas.App.Entities;
+
+namespace Pitangueiros.GuardioesDasQuentinhas.App.Services.Impl.Mappers
+{
+    public class UsuarioMapperProfile: Profile, IPerfilMapeador
+    {
+        public UsuarioMapperProfile()
+        {
+            CreateMap<Usuario, UsuarioOutputDto>();
+        }
+    }
+}
